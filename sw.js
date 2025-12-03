e6396e89c6082240fb75ad4a63369dfb50b3ba82
Khirul Anuar <mdkhirulanuar@gmail.com>
2025-12-03 07:26:58 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "career-align-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./dashboard.html",
+  "./css/style.css",
+  "./js/main.js",
+  "./js/dashboard.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => null);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request);
+    })
+  );
+});
